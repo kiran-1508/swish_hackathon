449ee8e4565cc3a6ae59c720c2eb9232860ed21d
kiran-1508 <50281970+kiran-1508@users.noreply.github.com>
2019-08-10 17:10:19 +0530

    Update swish_hackathon.c

--- swish_hackathon.c
+++ swish_hackathon.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 void main()
 {
-  int i,y,a[2];
+  int i,y=0,a[2];
   for(i=0;i<2;i++)
   {
     scanf("%d",&a[i]);
   }
   while(a[0] != 0)
   {
-    y = a[0];
-    a[0] = ((a[1] / 100) * a[0]);
-    if(a[0] == 0)
-    {
-      break;
-    }
+    y += a[0];
+    a[0] = (a[1] * a[0])/100;
+
   }
   printf("%d",y);
 }
